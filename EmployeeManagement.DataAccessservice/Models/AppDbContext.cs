8972c0863c5378dd7085007d42c89930b3005573
Prateek Khankhoje <prateekkhankhoje@yahoo.com>
2023-07-16 19:52:55 +0530

    Initialize DB on startup and add seed data

--- EmployeeManagement.DataAccessservice/Models/AppDbContext.cs
+++ EmployeeManagement.DataAccessservice/Models/AppDbContext.cs
@@ -10,5 +10,14 @@ namespace EmployeeManagement.DataAccessservice.Models
         }
 
         public DbSet<EmployeeModel> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<EmployeeModel>().HasData(
+                new EmployeeModel() { Id=1, FirstName="Tom", LastName="Hanks", EmailId="TomHanks@gmail.com", Age=50},
+                new EmployeeModel() { Id=2, FirstName = "Matt", LastName = "Damon", EmailId = "MattDamon@gmail.com", Age = 40 },
+                new EmployeeModel() { Id = 3, FirstName = "Morgan", LastName = "Freeman", EmailId = "MorganFreeman@gmail.com", Age = 70 }
+                );
+        }
     }
 }
--- EmployeeManagement.DataAccessservice/Program.cs
+++ EmployeeManagement.DataAccessservice/Program.cs
@@ -14,7 +14,12 @@ builder.Services.AddScoped<IEmployeeRepository, SQLEmployeeRepository>();
 builder.Services.AddDbContextPool<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeDBConnection")));
 builder.Services.AddAutoMapper(typeof(EmployeeMapperProfile));
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.EnsureCreated();
 
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
