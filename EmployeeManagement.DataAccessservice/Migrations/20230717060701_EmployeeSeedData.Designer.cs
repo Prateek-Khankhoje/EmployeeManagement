262bcd5b97136d08d0e67d3e57d37846a9996592
Prateek Khankhoje <prateekkhankhoje@yahoo.com>
2023-07-17 11:50:43 +0530

    Employee Seed Data migration

--- EmployeeManagement.DataAccessservice/Migrations/20230717060701_EmployeeSeedData.Designer.cs
+++ EmployeeManagement.DataAccessservice/Migrations/20230717060701_EmployeeSeedData.Designer.cs
@@ -3,6 +3,7 @@ using EmployeeManagement.DataAccessservice.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EmployeeManagement.DataAccessservice.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230717060701_EmployeeSeedData")]
+    partial class EmployeeSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EmployeeManagement.DataAccessservice/Migrations/20230717060701_EmployeeSeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace EmployeeManagement.DataAccessservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeeSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employee",
+                columns: new[] { "Id", "Age", "EmailId", "FirstName", "LastName" },
+                values: new object[,]
+                {
+                    { 1, 50, "TomHanks@gmail.com", "Tom", "Hanks" },
+                    { 2, 40, "MattDamon@gmail.com", "Matt", "Damon" },
+                    { 3, 70, "MorganFreeman@gmail.com", "Morgan", "Freeman" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Employee",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
